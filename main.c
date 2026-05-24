c4525331c23ec6b895b7b6254c3199d8664db5d7
axion <axion@axion.com>
2026-05-24 21:47:13 +0700

    git init

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int half_life(int t)
+{
+    // 
+}
+
+int main()
+{
+    // input of element
+    // input of mass
+    // input of duration
+
+    // ...
+
+    // print final mass
+}
