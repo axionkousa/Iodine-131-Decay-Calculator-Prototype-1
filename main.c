28ab7ccfabcfdcd1d8613b94b6d03a91e38ebf54
axion <axion@axion.com>
2026-05-24 22:52:00 +0700

    The first prototype

--- main.c
+++ main.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 #include <math.h>
 
-int half_life(int t)
-{
-    // 
-}
+// This is a half-life decay simulation of elmeent iodine-131
+
+float half_life(float a);
 
 int main()
 {
-    // input of element
+    float initial_mass;
+    int days;
+    int t_half = 8;
+
+    printf("This is a half-life decay simulation of elmeent iodine-131");
+
     // input of mass
-    // input of duration
+    printf("Initial mass: ");
+    if(scanf("%f", initial_mass) != 1){
+        return 1;
+    }
 
-    // ...
+    // input of duration in days
+    printf("Duration in days: ");
+    if(scanf("%d", days) != 1){
+        return 1;
+    }
 
-    // print final mass
+    
+    // prints out final mass after duration
+}
+
+// Calculates mass, duration, and half-life of iodine-131
+float half_life(float a){
+    // the formula of half-life
+    // return the answer
 }
